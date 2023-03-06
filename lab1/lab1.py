077af23bdb07f51a26d70fe91f264e44251260c0
Fifus17 <filipdziurdzia2002@gmail.com>
2023-03-06 12:33:51 +0100

    algorithm done, but results are 10^-22

---
+++ lab1/lab1.py
@@ -0,0 +1,66 @@
+# Importy
+import numpy as np
+import time
+
+# Stałe
+
+# Wartości początkowe
+x_0 = 1
+x_1:np.longdouble = 1/3
+
+# Wspłóczynniki w zależności rekurencyjnej
+a = 3
+b = -10
+c = 3
+d = 0
+
+# Wartość parametru k
+k = 10
+
+# Typy zmiennych
+types = [np.single, np.double, np.longdouble]
+
+# Funkcje
+
+def lab1(x_k_1, x_k, k, k_max, type):
+    for type in types:
+
+        # Rekurencja w przód
+        start = time.time()
+        result1 = rekurencjaPrzod(x_k_1, x_k, k, k_max, type)
+        end = time.time()
+        print("Funckja: ", rekurencjaPrzod.__name__)
+        print("Typ: ", type)
+        print("Czas wykonania: ", end - start)
+        print("Wartość: ", result1)
+        print("------------------------------------")
+
+        # Rekurencja w tył
+        start = time.time()
+        result2 = rekurencjaTyl(result1[0], result1[1], k_max, type)
+        end = time.time()
+        print("Funckja: ", rekurencjaTyl.__name__)
+        print("Typ: ", type)
+        print("Czas wykonania: ", end - start)
+        print("Wartość: ", result2)
+        print("------------------------------------")
+
+# Funkcja do rekurencji w przód
+def rekurencjaPrzod(x_k_1, x_k, k, k_max, type) -> type[2]:
+    result = type((-b*x_k - a*x_k_1)/c)
+    if k == k_max:
+        return [x_k, result]
+    else:
+        return rekurencjaPrzod(x_k, result, k+1, k_max, type)
+
+# Funkcja do rekurencji w tył
+def rekurencjaTyl(x_k_1, x_k, k, type) -> type[2]:
+    result = type((-b*x_k - c*x_k_1)/a)
+    if k == 1:
+        return [result, x_k_1]
+    else:
+        return rekurencjaTyl(result, x_k_1, k-1, type)
+
+
+# Wywołanie głównej funkcji
+lab1(x_0, x_1, 1, k, types[1])
